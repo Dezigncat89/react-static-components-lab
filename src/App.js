c0f5c646918af63b875bc60912cb391e6cff727c
Elaina Sorenson <78282104+Dezigncat89@users.noreply.github.com>
2021-05-20 07:59:46 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React, { Component } from 'react';
+import React from "react"
+import CatComponent from "./CatComponent"
+import GraceHopperQuoteComponent from "./GraceHopperQuoteComponent"
+import MouseComponent from "./MouseComponent"
+
 
 class App extends React.Component {
 	render() {
-	
-		return (
-			<div className="App">
-				<CatComponent />
-				<GraceHopperQuoteComponent />
-				<MouseComponent />
-			</div>
-		);
+	  return (
+	    <div className="App">
+	      <CatComponent />
+	      <GraceHopperQuoteComponent />
+	      <MouseComponent />
+	     </div>
+	   );
 	}
 }
 
-export default App;
+export default App
